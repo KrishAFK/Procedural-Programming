979635d550d839cdd6cf5f29c5974a3ad0fcf55a
Krish Shah <141353008+KrishAFK@users.noreply.github.com>
2026-05-11 00:07:57 +0100

    Add files via upload

---
+++ Loops_Proj1_Primenumbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    int isPrime = 1; 
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 1) {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = 0; 
+                break;  
+            }
+        }
+    }
+
+    if (isPrime == 1) {
+        printf("%d is a prime number.", num);
+    } else {
+        printf("%d is not a prime number.", num);
+    }
+}
