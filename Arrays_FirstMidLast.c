da5be36142227b86822df5f39fcc95cfb14bf91c
Krish Shah <141353008+KrishAFK@users.noreply.github.com>
2026-05-15 00:43:08 +0100

    Add files via upload

---
+++ Arrays_FirstMidLast.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int array[5]={1,2,3,4,5};
+
+    printf("The first element of the array is : %d\n",array[0]);
+    printf("The middle element of the array is : %d\n",array[2]);
+    printf("The last element of the array is : %d\n",array[4]);
+}
